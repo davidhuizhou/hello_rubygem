3a38bb61db025310ee732c3b403443155b65e9ca
David Zhou <davidhuizhou@gmail.com>
2021-01-23 10:52:05 -0500

    Initial commit

---
+++ lib/hello_rubygem.rb
@@ -0,0 +1,6 @@
+require "hello_rubygem/version"
+
+module HelloRubygem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hello_rubygem/version.rb
@@ -0,0 +1,3 @@
+module HelloRubygem
+  VERSION = "0.1.0"
+end
